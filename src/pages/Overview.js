3ab4c005d3ce106eb2af8ffd0187b80a9ce0bf23
Finnvb <finnvb@gmail.com>
2022-10-28 13:41:08 +0200

    Update Overview.js

--- src/pages/Overview.js
+++ src/pages/Overview.js
@@ -6,9 +6,21 @@ function OverviewPage(){
     console.log(document);
     return <div>{document && (<p>{document.data.storytitle[0].text}</p>) }
     
-    <div>{document && (<p>{document.data.storycontent[0].text}</p>) }</div>
+  
+  
+    <div>{document && (<p>{document.data.storycontent[2].text}</p>) }</div>
     
     </div>
     }
+    // return (
+    //     <div>
+    //       {document.map(employee => {
+    //         return (
+    //             <div>{document && (<p>{employee.document.data.storycontent.text}</p>) }</div>
+    //         );
+    //       })}
+    //     </div>
+    //   );
+    // }
     
     export default OverviewPage;
